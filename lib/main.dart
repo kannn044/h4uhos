d46787b463ea1a1271a10c55ad07fa7ab3e9398b
Satit Riapit <siteslave@msn.com>
2018-06-03 00:36:06 +0700

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'dart:async';
-import 'dart:convert' show json;
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:h4u/screens/Services.dart';
-import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:h4u/screens/LoginScreen.dart';
 import 'package:h4u/screens/SplashScreen.dart';
@@ -40,46 +33,5 @@ void main() {
         '/hospital/register': (BuildContext context) => new HospitalRegister(),
       },
     ),
-
   );
 }
-
-//
-//
-//class SmartPhr extends StatefulWidget {
-//  @override
-//  State createState() => new SmartPhrState();
-//}
-//
-//class SmartPhrState extends State<SmartPhr> {
-//
-//  String _currentUser;
-//
-////  Future<Null> _handleSignOut() async {
-////    _googleSignIn.disconnect();
-////    setState(() {
-////      _currentPage = "LOGINPAGE";
-////    });
-////  }
-//
-//  @override
-//  void initState() {
-//    // TODO: implement initState
-//    super.initState();
-////    startTime();
-//  }
-//
-////  startTime() async {
-////    var _duration = new Duration(seconds: 2);
-////    return new Timer(_duration, navigationPage);
-////  }
-//
-//  void navigationPage() {
-//    Navigator.of(context).pushReplacementNamed('/login');
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container();
-//  }
-//}
